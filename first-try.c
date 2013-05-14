61087f106d469819ed69087e1c37d001cec99d0c
Scarlat Tiberiu <tiberiu.scarlat@cti.pub.ro>
2013-05-15 00:03:45 +0300

    Update first-try.c

--- first-try.c
+++ first-try.c
@@ -39,50 +39,50 @@ int main(int argc, char * * argv) {
 
     const int ARRAY_SIZE = 64;
     const int ARRAY_BYTES = ARRAY_SIZE * sizeof(float);
-
+   
     // generam un vector ( input) asupra host-ului
     float h_in[ARRAY_SIZE];
     for (int i = 0; i < ARRAY_SIZE; i++) {
-
+   
         h_in[i] = float(i);
-
+   
     }
-
+   
     float h_out[ARRAY_SIZE];
-
+   
     //GPU Memory pointers
-
+   
     float * d_in;
     float * d_out;
-
+   
     // allocate GPU memory
+   
+   cudaMalloc((void * * )) & d_in, ARRAY_BYTES);
+	cudaMalloc((void * * )) & d_out, ARRAY_BYTES);
 
-    cudaMalloc((void * * )) & d_in, ARRAY_BYTES);
-cudaMalloc((void * * )) & d_out, ARRAY_BYTES);
-
-// transfer catre GPU
-
-cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
-
-//lansam kernel
-
-square << < 1, ARRAY_SIZE >>> (d_out, d_in);
-// 64 de copi a kernel-ului pe 64 de thread-uri;
-
-
-// transfer de la GPU catre CPU
-
-cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
-
-for (int i = 0; i < ARRAY_SIZE; i++) {
-
-    printf("%f", h_out[i]);
-    printf(((i % 4) != 3) ? "\t" : "\n");
-
-}
-
-cudaFree(d_int);
-cudaFree(d_out);
-
-return 0;
+	// transfer catre GPU
+	
+	cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
+	
+	//lansam kernel
+	
+	square << < 1, ARRAY_SIZE >>> (d_out, d_in);
+	// 64 de copi a kernel-ului pe 64 de thread-uri;
+	
+	
+	// transfer de la GPU catre CPU
+	
+	cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
+	
+	for (int i = 0; i < ARRAY_SIZE; i++) {
+	
+	    printf("%f", h_out[i]);
+	    printf(((i % 4) != 3) ? "\t" : "\n");
+	
+	}
+	
+	cudaFree(d_int);
+	cudaFree(d_out);
+	
+	return 0;
 }
